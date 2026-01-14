206db4e015151f6fea4e1aabb09746ef42749b62
chaowenguo <chaowen.guo1@gmail.com>
2026-01-13 19:45:15 -0800

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -12,7 +12,7 @@ function respawn(_)
 
 const app = new Koa()
 app.use(list(import.meta.dirname)).use(serve(import.meta.dirname))
-child_process.spawn(path.join(import.meta.dirname, 'dotnet/dotnet'), [path.join(import.meta.dirname, 'Cli.dll'), 'start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58='])
+child_process.spawn('dotnet', [path.join(import.meta.dirname, 'Cli.dll'), 'start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58='])
 //child_process.spawn(path.join(import.meta.dirname, 'bitpingd'))
 respawn(child_process.spawn('node', [path.join(import.meta.dirname, 'script.js'), '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443', '--id', 'd' + '0'.repeat(63), '--version', await globalThis.fetch('https://app-updates.sock.sh/peerclient/script/version.txt').then(_ => _.json()), '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP']))
 app.listen(3000)
