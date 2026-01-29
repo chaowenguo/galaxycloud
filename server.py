864a49ee0b03773ae6a4672f907121eb9566a0f6
chaowenguo <chaowen.guo1@gmail.com>
2026-01-28 19:35:37 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-import asyncio, aiohttp.web, pathlib, uvloop, sys, math#, wizardgain, builtins, uuid
+import asyncio, aiohttp.web, pathlib, uvloop, sys, math, os#, wizardgain, builtins, uuid
 
 async def main():
     app = aiohttp.web.Application()
     app.add_routes([aiohttp.web.static('/', pathlib.Path(__file__).resolve().parent, show_index=True)])
     runner = aiohttp.web.AppRunner(app)
     await runner.setup()
-    site = aiohttp.web.TCPSite(runner, port=3000)
+    site = aiohttp.web.TCPSite(runner, port=os.getenv('PORT'))
     await site.start()
     #asyncio.create_task(wizardgain.run_client(builtins.str(uuid.uuid4()), 'chaowen.guo1@gmail.com', 'https://connector.wizardgain.com'))
     #while True:
