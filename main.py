8418fb7af3fef048546d1c9e4ac5136f4af925a2
chaowenguo <chaowen.guo1@gmail.com>
2026-09-17 22:35:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,6 @@ async def main():
     #while True:
     #    node = await asyncio.create_subprocess_exec('node', pathlib.Path(__file__).resolve().parent.joinpath('script.js'), '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443', '--id', 'galaxycloud', '--version', '54', '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP')
     #    await node.wait()
-    await asyncio.Event().wait()
+    await asyncio.Future()
     
 uvloop.run(main())
