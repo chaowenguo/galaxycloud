171ce6b74f538c07dcc53a557089a66bd3f75e30
chaowenguo <chaowen.guo1@gmail.com>
2026-09-17 23:32:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ async def main():
                     member.name = pathlib.Path(member.name).name
                     tar.extract(member, path=pathlib.Path(__file__).resolve().parent)
         await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('cli'), 'start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58=')
-        pathlib.Path.home().joinpath('.bitpingd').mkdir(parents=True, exist_ok=True)
-        #async with client.get('https://releases.bitping.com/bitpingd/update.json') as releases:
-        #     async with client.get((await releases.json()).get('platforms').get('linux-x86_64').get('url')) as response:
-        #            tar = tarfile.open(mode='r:gz', fileobj=io.BytesIO(await response.content.read()))
-        #            tar.extract('bitpingd', path=pathlib.Path(__file__).resolve().parent)
-        #await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('bitpingd'))
-        print(pathlib.Path.home(), flush=True)
+        bitpingd = pathlib.Path.home().joinpath('.bitpingd')
+        bitpingd.mkdir(parents=True, exist_ok=True)
+        shutil.move('/app/node.db', bitpingd)
+        async with client.get('https://releases.bitping.com/bitpingd/update.json') as releases:
+             async with client.get((await releases.json()).get('platforms').get('linux-x86_64').get('url')) as response:
+                    tar = tarfile.open(mode='r:gz', fileobj=io.BytesIO(await response.content.read()))
+                    tar.extract('bitpingd', path=pathlib.Path(__file__).resolve().parent)
+        await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('bitpingd'))
     #asyncio.create_task(wizardgain.run_client(builtins.str(uuid.uuid4()), 'chaowen.guo1@gmail.com', 'https://connector.wizardgain.com'))
     #while True:
     #    node = await asyncio.create_subprocess_exec('node', pathlib.Path(__file__).resolve().parent.joinpath('script.js'), '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443', '--id', 'galaxycloud', '--version', '54', '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP')
