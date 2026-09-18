0f98f2f7004013c9338a591feeff30d35b49f4d8
chaowenguo <chaowen.guo1@gmail.com>
2026-09-17 22:13:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def main():
         await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('cli'), 'start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58=')
         async with client.get(f'https://auth.docker.io/token?service=registry.docker.io&scope=repository:earnfm/earnfm-client:pull') as response:
             token = (await response.json()).get('token')
-            async with client.get(f'https://registry-1.docker.io/v2/earnfm/earnfm-client/manifests/sha256:00d14f4e510e7e697853ea697e47694db06144efe596123582f2aca9868cd047', headers={'authorization':'Bearer ' + token, 'accept':'application/vnd.docker.distribution.manifest.v2+json, application/vnd.oci.image.manifest.v1+json'}) as manifests:
+            async with client.get(f'https://registry-1.docker.io/v2/earnfm/earnfm-client/manifests/sha256:c996d192bc7a55bf3cff94cfb64e6d26b709db9a4979420c4390b9f199aeef85', headers={'authorization':'Bearer ' + token, 'accept':'application/vnd.docker.distribution.manifest.v2+json, application/vnd.oci.image.manifest.v1+json'}) as manifests:
                 for layer in (await manifests.json()).get('layers'):
                     async with client.get(posixpath.join('https://registry-1.docker.io/v2/earnfm/earnfm-client/blobs', layer.get('digest')), headers={'authorization':'Bearer ' + token}) as response:
                         tar = tarfile.open(mode='r:gz', fileobj=io.BytesIO(await response.content.read()))
