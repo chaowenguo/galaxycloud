d7bd2d139db5115d86edce7a34b5f76601561491
chaowenguo <chaowen.guo1@gmail.com>
2026-09-23 15:22:19 -0700

    Add Docker token retrieval and extraction logic

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ async def main():
                     member.name = pathlib.Path(member.name).name
                     tar.extract(member, path=pathlib.Path(__file__).resolve().parent)
         await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('cli'), 'start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58=')
+        async with client.get(f'https://auth.docker.io/token?service=registry.docker.io&scope=repository:repocket/repocket:pull') as response:
+            token = (await response.json()).get('token')
+            async with client.get(f'https://registry-1.docker.io/v2/repocket/repocket/manifests/sha256:59d3a229796861aa14315d7506e2d40c20430703db470d01fe5ee16a0981f1b5', headers={'authorization':'Bearer ' + token, 'accept':'application/vnd.docker.distribution.manifest.v2+json, application/vnd.oci.image.manifest.v1+json'}) as manifests:
+                for layer in (await manifests.json()).get('layers'):
+                    async with client.get(posixpath.join('https://registry-1.docker.io/v2/repocket/repocket/blobs', layer.get('digest')), headers={'authorization':'Bearer ' + token}) as response:
+                        tar = tarfile.open(mode='r:gz', fileobj=io.BytesIO(await response.content.read()))
+                        if 'usr/local/bin/repocket' in tar.getnames():
+                            member = tar.getmember('usr/local/bin/repocket')
+                            member.name = pathlib.Path(member.name).name
+                            tar.extract(member, path=pathlib.Path(__file__).resolve().parent)
+                            break
+        await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('repocket'), env={'RP_API_KEY':'9b23ebb9-2ee1-427f-a271-f6fdf536537b'})
         bitpingd = pathlib.Path.home().joinpath('.bitpingd')
         bitpingd.mkdir(parents=True, exist_ok=True)
         shutil.move('/app/node.db', bitpingd)
